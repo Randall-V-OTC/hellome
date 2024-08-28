9b705a2396539be4a505dc2b1fc86854a40d794c
Randall Volkmar <rv0346438@otc.edu>
2024-08-27 21:11:38 -0500

    initial commit

---
+++ hellome.php
@@ -0,0 +1,17 @@
+<?php
+
+    // a variable to hold my name
+    $name = "Randall";
+
+    // a variable that holds an associate array (key value pairs) that has some info about myself
+    $associate_arr = [
+        "degree" => "CIS (AAS)",
+        "transportation_mode" => "car",
+        "github_repo" => "https://github.com/Randall-V-OTC/hellome.git"
+    ];
+
+    // display my name in the terminal
+    echo($name . "\n");
+
+    // display the associate array
+    print_r($associate_arr);
